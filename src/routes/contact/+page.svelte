4c59f90b4553f12f03ff631e3bff174623a83b1f
Rogério Cunha <rogerio.cunha@gmail.com>
2023-02-18 12:02:15 -0300

    alteracoes

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -11,11 +11,15 @@
 <h1>Contato</h1>
 
 <p>Rogério Ferreira da Cunha</p>
-<p>Email de contato: <bold>jusperitia@gmail.com</bold></p>
 
-<a href=https://br.linkedin.com/in/rogerio-cunha-1131ba>Perfil no Linkedin</a>
-<a href=https://wa.me/5521981257759>WhatsApp</a>
 
+<ul>
+	<li>Email: <bold>jusperitia@gmail.com</bold></li>
+	<li><a href=https://br.linkedin.com/in/rogerio-cunha-1131ba>Perfil no Linkedin</a></li>
+	<li><a href=https://wa.me/5521981257759>WhatsApp</a></li>
+
+
+</ul>
 
 <Callout>Informações Pessoais devem seguir o disposto na Lei de Geral de Proteção de Dados</Callout>
 
